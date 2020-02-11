f4f7d0e3dac254c8083330bbbb7713f9bcbf6acd
KannanVS <kannansanthanam97@gmail.com>
2020-02-11 16:02:26 +0100

    read file

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
+import * as fs from 'fs';
+// fs.readFileSync('foo.txt','utf8');
+fs.readFile('check.txt', 'utf8', function(err, data) {
+    if (err) throw console.log('there is an error');
+    console.log(data);
+});
 console.log('check');
